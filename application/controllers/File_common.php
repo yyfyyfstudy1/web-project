3a6780785b8f47e17661728a2c5bfead00dd6b43
yyfyyfstudy1 <88156048+yyfyyfstudy1@users.noreply.github.com>
2022-04-15 22:27:21 +0800

    对gallery功能的重构

--- application/controllers/File_common.php
+++ application/controllers/File_common.php
@@ -75,56 +75,6 @@ class file_common extends CI_Controller
 	public function filterData()
 		{
 			
-			if(!$this->session->userdata('logged_in')){
-				$this->load->view('template/header');
-			}else{
-				$this->load->model('file_model');
-				$img_name = $this->file_model->print_img_profile($this->session->userdata('username'));
-	
-				$var = array();
-				foreach($img_name->result() as $row)
-					   {
-						 
-					   $var[] = ''.base_url().'uploads_profile/'.$row->filename.'';
-	   
-					   }
-				$data_use['error'] = $var;
-				$this->load->view('template/header',$data_use);
-			} 
-			if (!$this->session->userdata('logged_in'))//check if user already login
-			{	
-				if (get_cookie('remember')) { // check if user activate the "remember me" feature  
-					$username = get_cookie('username'); //get the username from cookie
-					$password = get_cookie('password'); //get the username from cookie
-					if ( $this->user_model->login($username, $password) )//check username and password correct
-					{
-						$user_data = array('username' => $username,'logged_in' => true );
-						$this->session->set_userdata($user_data); //set user status to login in session
-	
-	
-	
-						$this->load->model('file_model');  
-				
-						$username = $this->session->userdata('username');
-						$data = $this->file_model->print_common_img();
-						$var = array();
-						foreach($data->result() as $row)
-						{
-					  
-						$var[] = $row->filename;
-	
-						}
-						$data_use['img_src'] = $var;
-						$data_use['user_name']= $username;
-						$data_use['if_likes'] = 1;
-	
-						$this->load->view('file_common',$data_use ); //if user already logined show upload page
-	
-					}
-				}else{
-					redirect('login'); //if user already logined direct user to home page
-				}
-			}else{
 				$this->load->model('file_model');  
 						if($this->input->post('category')=='recommend'){
 							$data = $this->file_model->print_common_img2();
@@ -135,85 +85,108 @@ class file_common extends CI_Controller
 						$username = $this->session->userdata('username');
 						
 	
-						$data_use['files'] = $data->result();
+						$files = $data->result();
 	
-						$data_use['user_name']= $username;
-						$data_use['if_likes'] = 1;
-	
-						$this->load->view('file_common',$data_use );
-	
-	
-			}
-			$this->load->view('template/footer');
+						 /**
+         * 提取富文本中的纯文字
+        * addtime 2020年8月10日 09:45:20
+        * @param [type] $string 字符串
+        * @return void
+        */
+        function StringExtractionText($string)
+        {
+        if($string){
+            // 把一些预定义的 HTML 实体转换为字符
+            // 预定义字符是指:<,>,&等有特殊含义(<,>,用于链接签,&用于转义),不能直接使用
+            $html_string = htmlspecialchars_decode($string);
+            // 将空格去除
+            $content = str_replace(" ", " ", $html_string);
+            // 去除字符串中的 HTML 标签
+            $contents = strip_tags($content);
+            // 设置截取的字数
+            $num = 100;
+            // 利用三元运算判断文字是否超出设置的字数进行截取
+            return mb_strlen($contents,'utf-8') > $num ? mb_substr($contents, 0, $num, "utf-8").'...' : mb_substr($contents, 0, $num, "utf-8");
+        }else{
+            return false;
+        }
+        }
 
 
+    foreach($files as $file){
 
+        $query = array(
+            'id' => '7876',
+            'name' => $file->filename // 加一个中文参数的示例
+        );
+        $url = ''.base_url().'comment_page?' . http_build_query($query); // 这样可以自动转义url不允许的字符
+        
+        $content_use = StringExtractionText($file->content);
+
+        echo '
+        
+			
+			
+		<div class="big-container" >
+		<a href="'.$url.'">
+			<div class="title">
+			<p>
+			<font size="3">
+			<b>
+			'.$file->title.'
+			<b>
+			</font>
+			</p>
+		
+			</div>
+	
+			<div class="left-img">
+				<img src = "'.base_url().'uploads/'. $file->filename.'" width="137px" height="100px">
+			
+			</div>
+		</a>
+			<div class="right-container">
+				<div class="content">
+					<font size="2" color="gray">
+						'.$content_use.'
+					
+					</font>
+
+				</div>
+				
+				<div class="likes">
+					
+						
+					<image src="'.base_url().'assets/img/赞.png" width="30px" height="30px" style="float:left" class="rateButton" onclick="likeAjax('.$file->id.')"></image>
+			
+					<p style="margin-left: 7px; float:left; margin-top:5px">'.$file->user_likes.'<p>
+					
+
+				<form action="'.base_url().'File_common/userLikes" method="post" name="LikeForm" >
+					<input  name="dislikes" type="hidden" value='.$file->id.'>
+					<image src="'.base_url().'assets/img/踩.png" width="25px" height="25px" style="margin-left: 40px;margin-top:5px; float:left" id="submitableimag"></image>
+					<p style="margin-left: 7px; float:left; margin-top:5px">'.$file->dislikes.'<p>
+				
+					<p style="margin-left: 27px; float:left; margin-top:5px">'.$file->username.'<p>
+				</form>
+				</div>
+			
+			</div>
+			
+		</div>
+		
+          
+        
+        ';
+        
+    }
 
 		}
 
 		//定义点赞的方法
 		public function userLikes(){
 
-			if(!$this->session->userdata('logged_in')){
-				$this->load->view('template/header');
-			}else{
-				$this->load->model('file_model');
-				$img_name = $this->file_model->print_img_profile($this->session->userdata('username'));
-	
-				$var = array();
-				foreach($img_name->result() as $row)
-					   {
-						 
-					   $var[] = ''.base_url().'uploads_profile/'.$row->filename.'';
-	   
-					   }
-				$data_use['error'] = $var;
-				$this->load->view('template/header',$data_use);
-			} 
-			if (!$this->session->userdata('logged_in'))//check if user already login
-			{	
-				if (get_cookie('remember')) { // check if user activate the "remember me" feature  
-					$username = get_cookie('username'); //get the username from cookie
-					$password = get_cookie('password'); //get the username from cookie
-					if ( $this->user_model->login($username, $password) )//check username and password correct
-					{
-						$user_data = array('username' => $username,'logged_in' => true );
-						$this->session->set_userdata($user_data); //set user status to login in session
-	
-	
-	
-						$this->load->model('file_model');  
-				
-						$username = $this->session->userdata('username');
-
-						if($this->input->post('likes')){
-							$likes_id = $this->input->post('likes');
-							echo($likes_id);
-							$this->file_model->likes_add($likes_id);
-						}
-						
-						if($this->input->post('dislikes')){
-							$dislikes_id = $this->input->post('dislikes');
-						}
-
-
-						$data = $this->file_model->print_common_img();
-					
-						$data_use['files'] = $data->result();
-
-	
-						$data_use['user_name']= $username;
-	
-						$this->load->view('file_common',$data_use );
-	
-	
-						$this->load->view('file_common',$data_use ); //if user already logined show upload page
-	
-					}
-				}else{
-					redirect('login'); //if user already logined direct user to home page
-				}
-			}else{
+		
 				$this->load->model('file_model');  
 					
 					$username = $this->session->userdata('username');
@@ -227,23 +200,7 @@ class file_common extends CI_Controller
 							//如果点赞过
 							if($this->file_model->if_likes($username, $likes_id)){
 
-								if($this->input->post('category')=='recommend'){
-									$data = $this->file_model->print_common_img2();
-								}else{
-									$data = $this->file_model->print_common_img($this->input->post('category'));
-								}
-								
-								
-								
-								$data_use['files'] = $data->result();
-			
-								$data_use['user_name']= $username;
-
-								$data_use['if_likes'] = 0;
-			
-								$this->load->view('file_common',$data_use );
-			
-
+								echo '搞什么飞机，你点赞过这个了';
 
 
 							}else{
@@ -251,31 +208,11 @@ class file_common extends CI_Controller
 								$this->file_model->insert_llike($username, $likes_id);
 								$this->file_model->likes_add($likes_id);
 
-								if($this->input->post('category')=='recommend'){
-									$data = $this->file_model->print_common_img2();
-								}else{
-									$data = $this->file_model->print_common_img($this->input->post('category'));
-								}
-								
-								
-								
-								$data_use['files'] = $data->result();
-			
-								$data_use['user_name']= $username;
-
-								$data_use['if_likes'] = 1;
-			
-								$this->load->view('file_common',$data_use );
-			
-
 							}
 								
 
 						}
-						
-	
-			}
-			$this->load->view('template/footer');
+					
 
 		}
 
--- application/views/file_common.php
+++ application/views/file_common.php
@@ -1,23 +1,24 @@
 <div class="row justify-content-center">
-    <div class="form-group">
+    <div style="height: max-content;">
  <!-- <h1>  Hello <font style="color:red; font-size :70px; "><?php echo $user_name ?></font>You can browse friends' pictures here</h1> -->
 
     <!-- 提交文章分类信息的表单 -->
-    <?php echo form_open(base_url().'File_common/filterData'); ?>
+    <!-- 使用ajax提交分类数据 -->
     <div class="btn-group" role="group" aria-label="Basic outlined example" style="margin-left: 20px; margin-top:10px">
-    <button type="submit" class="btn btn-light" value="recommend" name="category"style="width:110px;  border-radius: 15px;">recommend</button>
-                    <button type="submit" class="btn btn-light" value="docker" name="category" style="width:80px; border-radius: 15px;margin-left:10px;">docker</button>
-                    <button type="submit" class="btn btn-light" value="java" name="category" style="width:80px; margin-left:10px;  border-radius: 15px;">java</button>
-                    <button type="submit" class="btn btn-light" value="python" name="category"style="width:80px; margin-left:10px;  border-radius: 15px;">python</button>
-                    <button type="submit" class="btn btn-light" value="cloud" name="category"style="width:80px; margin-left:10px;  border-radius: 15px;">cloud</button>
-                    <button type="submit" class="btn btn-light" value="java" name="category" style="width:80px; margin-left:10px;  border-radius: 15px;">java</button>
-                    <button type="submit" class="btn btn-light" value="python" name="category"style="width:80px; margin-left:10px;  border-radius: 15px;">python</button>
-                    <button type="submit" class="btn btn-light" value="cloud" name="category"style="width:80px; margin-left:10px;  border-radius: 15px;">cloud</button>
+                    <button type="submit" class="btn btn-light" value="recommend" name="category"style="width:110px;  border-radius: 15px;" onclick="startAjax('recommend')">recommend</button>
+                    <button type="submit" class="btn btn-light" value="docker" name="category" style="width:80px; border-radius: 15px;margin-left:10px;" onclick="startAjax('docker')">docker</button>
+                    <button type="submit" class="btn btn-light" value="java" name="category" style="width:80px; margin-left:10px;  border-radius: 15px;" onclick="startAjax('java')">java</button>
+                    <button type="submit" class="btn btn-light" value="python" name="category"style="width:80px; margin-left:10px;  border-radius: 15px;" id="test1" onclick="startAjax('python')">python</button>
+                    <button type="submit" class="btn btn-light" value="cloud" name="category"style="width:80px; margin-left:10px;  border-radius: 15px;" id="test1" onclick="startAjax('cloud')">cloud</button>
+                    <button type="submit" class="btn btn-light" value="java" name="category" style="width:80px; margin-left:10px;  border-radius: 15px;" id="test1" onclick="startAjax('java')">java</button>
+                    <button type="submit" class="btn btn-light" value="python" name="category"style="width:80px; margin-left:10px;  border-radius: 15px;" id="test1" onclick="startAjax('python')">python</button>
+                    <button type="submit" class="btn btn-light" value="cloud" name="category"style="width:80px; margin-left:10px;  border-radius: 15px;" id="test1" onclick="startAjax('cloud')">cloud</button>
                     
     </div>      
          
-    <?php echo form_close(); ?>
+  
 
+    <div id="result2"></div>
     <?php 
     
         if($if_likes==0){
@@ -42,107 +43,8 @@
 
     <div class="all-container" id="ratings">
    
-     
-    <?php 
-
-        /**
-         * 提取富文本中的纯文字
-        * addtime 2020年8月10日 09:45:20
-        * @param [type] $string 字符串
-        * @return void
-        */
-        function StringExtractionText($string)
-        {
-        if($string){
-            // 把一些预定义的 HTML 实体转换为字符
-            // 预定义字符是指:<,>,&等有特殊含义(<,>,用于链接签,&用于转义),不能直接使用
-            $html_string = htmlspecialchars_decode($string);
-            // 将空格去除
-            $content = str_replace(" ", " ", $html_string);
-            // 去除字符串中的 HTML 标签
-            $contents = strip_tags($content);
-            // 设置截取的字数
-            $num = 100;
-            // 利用三元运算判断文字是否超出设置的字数进行截取
-            return mb_strlen($contents,'utf-8') > $num ? mb_substr($contents, 0, $num, "utf-8").'...' : mb_substr($contents, 0, $num, "utf-8");
-        }else{
-            return false;
-        }
-        }
-
-
-    foreach($files as $file){
-
-        $query = array(
-            'id' => '7876',
-            'name' => $file->filename // 加一个中文参数的示例
-        );
-        $url = ''.base_url().'comment_page?' . http_build_query($query); // 这样可以自动转义url不允许的字符
-        
-        $content_use = StringExtractionText($file->content);
-
-        echo '
-        
-        
-        
-    <div class="big-container" >
-    <a href="'.$url.'">
-        <div class="title">
-        <p>
-        <font size="3">
-        <b>
-        '.$file->title.'
-        <b>
-        </font>
-        </p>
-       
-        </div>
-   
-        <div class="left-img">
-             <img src = "'.base_url().'uploads/'. $file->filename.'" width="137px" height="100px">
-        
-        </div>
-    </a>
-        <div class="right-container">
-            <div class="content">
-                <font size="2" color="gray">
-                    '.$content_use.'
-                
-                </font>
-
-            </div>
-            
-            <div class="likes">
-                <form action="'.base_url().'File_common/userLikes" method="post" id="submitableimag'.$file->id.'">
-                <input  name="Ilikes" type="hidden" value="'.$file->id.'">
-                <image src="'.base_url().'assets/img/赞.png" width="30px" height="30px" style="float:left" class="rateButton" id="submitableimag'.$file->id.'"></image>
-    
-                <p style="margin-left: 7px; float:left; margin-top:5px">'.$file->user_likes.'<p>
-                </form>
-
-
-
-              <form action="'.base_url().'File_common/userLikes" method="post" name="LikeForm" >
-                <input  name="dislikes" type="hidden" value='.$file->id.'>
-                <image src="'.base_url().'assets/img/踩.png" width="25px" height="25px" style="margin-left: 40px;margin-top:5px; float:left" id="submitableimag"></image>
-                <p style="margin-left: 7px; float:left; margin-top:5px">'.$file->dislikes.'<p>
-               
-                <p style="margin-left: 27px; float:left; margin-top:5px">'.$file->username.'<p>
-                </form>
-            </div>
-        
-        </div>
-        
-    </div>
-    
-          
-        
-        ';
-        
-    }
-
-      
-    ?>
+     <!-- 此处用Ajax渲染文章列表 -->
+    <div id="result1"></div>
 
        
 
@@ -154,22 +56,34 @@
 
     </div>
 </div>
-
+<script src="https://cdn.staticfile.org/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
 <script>
+    
+    //页面刚加载的时候，发送Ajax请求获取推荐文章列表
+    $(document).ready(function(){
+    
+    startAjax('recommend');
+
+    });
+
+    // 退出当前页面储存用户浏览的信息
+    $(window).on("beforeunload", function() { 
+    // your codes
+    $.cookie('scrollTop',$(window).scrollTop());
+    console.log("this will be triggered");
+        
+    });
+
+
+
     // 通过代理获取相应的图片，提交相应的表单
     $('#ratings').on('click', '.rateButton', function(e){
         console.log($(this).attr('id'))
         let formId = $(this).attr('id')
-        
         $('#'+formId).submit();
     });
     
     
-   $('#submitableimag').click(function(){
-       console.log('11111')
-      $('#myform').submit();
-   });
-
    var timer;
 
     $(function () {
@@ -181,6 +95,34 @@
     }, 2000);
 
     })
+   
+    
+    // 拿到按钮点击的value，再发送ajax请求
+    function startAjax(value){
+			
+        $.ajax({
+        url:"<?php echo base_url(); ?>File_common/filterData",
+        method:"POST",
+        data:{category:value},
+        success:function(data){
+            $('#result1').html(data);
+        }
+        })
+	};
+
+
+    function likeAjax(value){
+
+        $.ajax({
+        url:"<?php echo base_url(); ?>File_common/userLikes",
+        method:"POST",
+        data:{Ilikes:value},
+        success:function(data){
+            $('#result2').html(data);
+        }
+        })
+
+    }
 
 
 </script>
